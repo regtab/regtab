9e794f903e0fb6047af26cebac8b1cf905cd4127
Alexey O. Shigarov <shigarov@gmail.com>
2024-04-02 19:31:01 +0800

    public

--- src/main/java/com/regtab/core/rtl/interpreter/pattern/BasePattern.java
+++ src/main/java/com/regtab/core/rtl/interpreter/pattern/BasePattern.java
@@ -6,9 +6,9 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-public abstract class BasePattern {
+abstract class BasePattern {
     @Getter
-    public final ParseTree tree;
+    private final ParseTree tree;
 
     BasePattern(ParseTree tree) {
         this.tree = tree;
--- src/main/java/com/regtab/core/rtl/interpreter/pattern/TablePattern.java
+++ src/main/java/com/regtab/core/rtl/interpreter/pattern/TablePattern.java
@@ -1,15 +1,12 @@
 package com.regtab.core.rtl.interpreter.pattern;
 
-import com.regtab.core.rtl.RTLPattern;
-import lombok.AccessLevel;
+import lombok.NonNull;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import com.regtab.core.rtl.parser.RTLParser.*;
-import lombok.NonNull;
-import lombok.Setter;
 
 public final class TablePattern extends BasePattern {
     public TablePattern(@NonNull TableContext context) {
